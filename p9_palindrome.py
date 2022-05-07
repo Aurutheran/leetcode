2170beb2197eccbc700055934e7646a7b40f633d
Aurutheran <74684383+Aurutheran@users.noreply.github.com>
2022-05-06 23:10:15 -0400

    Update p9_palindrome.py

--- p9_palindrome.py
+++ p9_palindrome.py
@@ -1,15 +1,13 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        temp = ""
+        
         
         stringnum = str(x)
-        
         size = len(stringnum)
+        i=0
+        while (i<size-1-i):
+            if(stringnum[i] is not stringnum[size-1-i]):
+                return False
+            i = i+1
+        return True
         
-        while (size-1>=0):
-            temp = temp + str(stringnum[size-1])
-            size = size-1
-        
-        if (temp == str(x)):
-            return True
-        return False
