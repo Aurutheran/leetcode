62e7afe4a0166b6e98c6df2b60a964bf4eb9138e
Aurutheran <74684383+Aurutheran@users.noreply.github.com>
2022-05-12 22:17:03 -0400

    Update p141_linkedlistcycle.py

--- p141_linkedlistcycle.py
+++ p141_linkedlistcycle.py
@@ -7,15 +7,15 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         
-        if (head == None):
+        if (head == None): #IF there is no head than we dont got a cycle
             return False
-        elif (head.next == None):
+        elif (head.next == None): #If there is no next head, there isn't a cycle
             return False
         
         h1=h2=head
-        while (h2 and h2.next):
-            h1,h2 = h1.next,h2.next.next
+        while (h2 and h2.next):   #We check if h2 and the next head exist to confirm cycle of 2+ size
+            h1,h2 = h1.next,h2.next.next    #We keep looping to see if h1 and h2 equal each other as we go to the next heads
             if (h1 == h2):
-                return True
-        return False
+                return True    #Eventually, they should equal the same value
+        return False   #If there isnt a h2 or a new h2 value after, we break
             
