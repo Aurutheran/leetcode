326e2275671d65c995c7b8e8248b075ca1792abc
Aurutheran <74684383+Aurutheran@users.noreply.github.com>
2022-06-04 22:20:31 -0400

    Create hackerrank_subarraydivision1.py

---
+++ hackerrank_subarraydivision1.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+def birthday(s, d, m):
+    # Write your code here
+    i = 0 #I = 0 for the start of array
+    j = m #J = m and is the length for the full subarray
+    lists = []
+    amount = 0
+    while j<= len(s):
+        if sum(s[i:j])== d:
+            amount += 1
+        i+=1
+        j+=1
+    return amount
+    
+    
+        
+        
+            
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
