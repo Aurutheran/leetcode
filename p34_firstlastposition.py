40ae5083692bc7e54cab9e5b42844c92cd23a18e
Aurutheran <74684383+Aurutheran@users.noreply.github.com>
2022-06-06 02:52:42 -0400

    Create p34_firstlastposition.py

---
+++ p34_firstlastposition.py
@@ -0,0 +1,15 @@
+class Solution:
+	def searchRange(self, nums: List[int], target: int) -> List[int]:
+		first = -1
+		second = -1
+		for i in range(len(nums)):
+			
+			if nums[i] == target:
+				if (first == -1):
+					first = i
+				else:
+					second = i
+		return [first, second]
+					
+					
+		
