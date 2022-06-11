cfc890848fa4900d383be42b5e85c08b3c292b21
Aurutheran <74684383+Aurutheran@users.noreply.github.com>
2022-06-11 00:23:34 -0400

    Update hackerrank_countergame.py

--- hackerrank_countergame.py
+++ hackerrank_countergame.py
@@ -19,7 +19,24 @@ def counter_game(n):
     winner = ["Louise","Richard"]  
     
     count = 0
+    vals = n
     
+    if (vals==1):
+        return winner[0]
+    
+    
+    while(vals != 1):
+        moduli = vals - 2**(math.log(vals)//math.log(2))
+    
+        if (moduli != 0):
+            vals = moduli
+        else:
+            vals = vals/2
+        
+        if vals != 1:
+            count += 1
+        
+    return winner[count%2]
         
 
     
