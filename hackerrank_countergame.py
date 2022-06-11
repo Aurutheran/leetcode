9fd52ef7467a985bddb4c951db7b0547dd334b21
Aurutheran <74684383+Aurutheran@users.noreply.github.com>
2022-06-10 23:52:49 -0400

    Update hackerrank_countergame.py

--- hackerrank_countergame.py
+++ hackerrank_countergame.py
@@ -13,26 +13,13 @@ import sys
 # The function accepts LONG_INTEGER n as parameter.
 #
 
-def get_mask(n):
-    return 1 << math.floor(math.log(n,2))
 
 def counter_game(n):
-    winner = 0
-    while n != 1:
-        mask = get_mask(n)
-        if mask == n:
-            n = n/2
-        else:
-            n -= mask
-            
-        winner ^= 1
     
+    winner = ["Louise","Richard"]  
     
-    if winner == 1:
-        return "Louise"
-    elif winner == 0:
-        return "Richard"
-        
+    count = 0
+    
         
 
     
