ffcab0bccf94222a4d958e3432ad651edd0cf4b1
Aurutheran <74684383+Aurutheran@users.noreply.github.com>
2022-06-11 23:39:43 -0400

    Create hackerrank_RecursiveDigitSum.py

---
+++ hackerrank_RecursiveDigitSum.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superDigit' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING n
+#  2. INTEGER k
+#
+
+def superDigit(n, k):
+    # Write your code here
+    
+    
+    if len(str(n)) == 1:
+        return n
+    
+    total = 0
+    
+    for each in str(n):
+        total += int(each)
+    
+    if len(str(total)) == 1:
+        return int(total)
+    
+    superDigit(total,len(str(total)))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = first_multiple_input[0]
+
+    k = int(first_multiple_input[1])
+
+    result = superDigit(n, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
